414fb3fef299dac81fd6079b59fd772c1383dd6d
Daniel Rodrigues de Vargas <101740830+dan1el074@users.noreply.github.com>
2023-04-28 16:49:44 -0300

    Upload dos arquivos atualizados

---
+++ index.php
@@ -0,0 +1,186 @@
+<!DOCTYPE html>
+<html class="no-js" lang="en">
+    <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/base.css">
+    <link rel="stylesheet" href="css/main.css">
+
+    <link rel="apple-touch-icon" sizes="180x180" href="images/icons/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="images/icons/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="images/icons/favicon-16x16.png">
+    <link rel="manifest" href="site.webmanifest">
+
+    <title>Xpert - Suprimentos para Impressão</title>
+    </head>
+
+    <body id="top">
+        <div id="preloader">
+            <div id="loader"></div>
+        </div>
+
+        <header class="s-header">
+            <a href="https://api.whatsapp.com/send?phone=5554992739630" class="btn-whatsapp-pulse btn-whatsapp-pulse-border" target="_blank">
+                <i class="fab fa-whatsapp"></i>
+            </a>
+
+            <div class="header-logo">
+                <a class="site-logo" href="">
+                    <img src="images/logo.svg" alt="Homepage">
+                </a>
+            </div>
+
+            <nav class="header-nav-wrap">
+                <ul class="header-main-nav">
+                    <li class="current"><a class="smoothscroll" href="#home" title="Home">Home</a></li>
+                    <li><a class="smoothscroll" href="#produtos" title="Produtos">Produtos</a></li>
+                    <li><a class="smoothscroll" href="#sobre" title="Sobre">Sobre</a></li>
+                    <li><a class="smoothscroll" href="#contato" title="Contato">Contato</a></li>	
+                </ul>
+            </nav>
+
+            <a class="header-menu-toggle" href="#"><span>Menu</span></a>
+        </header>
+
+        <section id="home" class="s-intro target-section">
+            <div class="row intro-content">
+                <div class="column large-9 mob-full intro-text">
+                    <h3>A sua escolha em</h3>
+                    <h1>
+                        SUPRIMENTOS <br>
+                        PARA <br>
+                        IMPRESSÃO
+                    </h1>
+                </div>
+
+                <div class="intro-scroll">
+                    <a href="#produtos" class="intro-scroll-link smoothscroll">
+                        Role para ver mais
+                    </a>
+                </div>
+
+                <div class="intro-grid"></div>
+                <div class="intro-pic"></div>
+            </div>
+        </section>
+
+        <section id="produtos" class="s-works target-section">
+            <img id="image" src="./images/lines.svg">
+            
+            <div class="row heading-block heading-block--center" data-aos="fade-up">
+
+                <div class="column large-full">
+                    <h2 class="section-heading section-heading--centerbottom">Produtos</h2>
+
+                    <div class="img-post">
+                        <img src="images/base-adf-m1191651-1.png">
+                        <a href="https://api.whatsapp.com/send?phone=5554992739630">FAÇA SEU PEDIDO</a>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section id="sobre" class="s-about target-section">
+            <div class="about-me">
+                <div class="row heading-block" data-aos="fade-up">
+                    <div class="column large-full">
+                        <h2 class="section-heading">Informações</h2>
+                    </div>
+                </div>
+
+                <div class="row about-me__content" data-aos="fade-up">
+                    <div class="column large-full about-me__text">
+                        <p class="lead">
+                        Nosso primeiro produto, BASE DE ADF PARA RICOH SP 3510SF M1191651, é feito 
+                        em outro país e depois exportado para o Brasil. Sendo um produto paralelo, 
+                        garante qualidade assim como o original e em seus moldes para servir em 
+                        qualquer impressora do modelo RICOH 3510SF.
+                        </p>
+
+                        <p>
+                        *Vale lembrar que se trata de uma peça única (apenas BASE DE ADF PARA RICOH 
+                        SP 3510SF M1191651), sendo vendido em uma caixa com 10 unidades e não de 
+                        forma unitária. Não conta com o restante do ADF assim como conjunto de 
+                        engrenagens, plásticos que os envolve e demais itens da impressora.
+                        </p>
+
+                        <p>
+                        A Xpert - Comércio de Equipamentos de Impressão, é uma empresa localizada na 
+                        região norte do Rio Grande de Sul e tem como principal objetivo oferecer 
+                        produtos relacionados a impressoras que, por muitas vezes, não se encontra 
+                        disponível em nosso mercado nacional.
+                        </p>
+
+                        <p>
+                        Contando com anos de experiência na área, sabemos exatamente a sua necessidade 
+                        e trabalhamos com seriedade, oferecendo garantia, nota fiscal de todos os 
+                        produtos que temos disponível em nosso estoque e atendemos demandas para todo o 
+                        Brasil.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="s-testimonials">
+            <div class="row heading-block" data-aos="fade-up">
+                <div class="column large-full">
+                    <h2 class="section-heading">DEPOIMENTOS</h2>
+                </div>
+            </div>
+        
+            <div class="row testimonials" data-aos="fade-up">
+                <div class="column large-full testimonials__slider">
+                    <div class="testimonials__slide">
+                        <p>“Atuando diariamente no setor de suporte à impressoras, por diversas vezes nos deparamos sem peças para reposição em equipamentos
+                            danificados, neste contexto encontramos a Xpert, com peças de alta qualidade e durabilidade.”</p>
+                        <div class="testimonials__info">
+                            <img src="images/avatars/lucas.jpg" alt="Author image" class="testimonials__avatar">
+                            <cite class="testimonials__cite">
+                                <strong>Lucas Gazda</strong>
+                                <span>Analista de Suporte, Gazda</span>
+                            </cite>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section id="contato" class="s-contact target-section">
+            <div class="row heading-block" data-aos="fade-up">
+                <div class="column large-full">
+                    <h2 class="section-heading">ENTRE EM CONTATO</h2>
+                </div>
+            </div>
+
+            <div class="row contact-main" data-aos="fade-up">
+                <div class="column large-full">
+                    <p class="section-desc email">
+                        <a href="mailto:comercial@xpertprint.com.br">comercial@xpertprint.com.br</a>
+                    </p>
+
+                    <p class="section-desc">
+                        Entre agora em contato com nossa equipe comercial pelo <span class="f-email">e-mail</span><a href="mailto:comercial@xpertprint.com.br" class="f-email-mobile">e-mail</a>, 
+                        whatsapp ou pelas nossas redes sociais.
+                    </p>
+                </div>
+            </div>
+        </section>
+
+        <footer>
+            <div class="row">
+                <div class="column large-full ss-copyright">
+                    <span>© Copyright 2022 - Xpert Comercio de Equipamentos de Impressão LTDA </span> 
+                    <span>Design by <a>Equipe de Desenvolvimento</a></span>
+                </div>            
+            </div>
+        </footer>
+
+        <script src="js/modernizr.js"></script>
+        <script src="js/pace.min.js"></script>
+        <script src="js/jquery-3.2.1.min.js"></script>
+        <script src="js/plugins.js"></script>
+        <script src="js/main.js"></script>
+
+    </body>
+</html>
