61d62539d1d5a1d600ad5776acf99b47b435a9b4
Daniel Vargas <daniel.rvargas10@gmail.com>
2023-06-05 11:30:15 -0300

    rename "index" php to html

---
+++ indexspm=a2g0o.home.superdeal.4.31c81c91GCrwXc&productIds=1005002553448/index.php
@@ -0,0 +1,284 @@
+<?php
+    $to = '';
+    $subject = 'Base de ADF - M1191651';
+    $messege = "
+        <html>
+            <head>
+                <title>Base de ADF - M1191651</title>
+            </head>
+
+            <body style=\"margin: 0; background-color: #f1f1f1;\">
+                <table align=\"center\" bgcolor=\"#ffffff\" border=\"0\" width=\"680\" style=\"border-collapse: collapse; margin: 20px auto\">
+                    <tr border=\"0\">
+                        <td style=\"background-color: #f1f1f1;\" width=\"100\" border=\"0\">
+                            &nbsp;
+                        </td>
+                    </tr>
+                
+                    <tr border=\"0\">
+                        <td border=\"0\" align=\"right\" style=\"vertical-align: bottom; padding: 0 110px 70px 0;\" width=\"680\" height=\"573\" background=\"https://raw.githubusercontent.com/dan1el074/dan1el074/main/.github/workflow/base-adf2.png\">
+                            <p style=\"display: inline-block; text-align: center; font-family: 'Roboto', sans-serif; font-weight: bold; font-size: 19px; line-height: 1.25; margin: 0\">
+                                BASE DE ADF PARA<br>
+                                <span style=\"color: #e56520\">RICOH SP</span> 3510SF M1191651*<br><br><br>
+                                
+                                <a href=\"https://loja.xpertprint.com.br/base-de-adf-p-ricoh-sp-3510sf-m1191651\" target=\"_blank\" style=\"background-color: #e56520; color: #fff; padding: 18px 33px; text-decoration: none; font-size: 22px; font-weight: 400; border-radius: 18px\">
+                                    FAÇA SEU PEDIDO
+                                </a>
+                            </p>
+                        </td>
+                    </tr>
+
+                    <tr border=\"0\">
+                        <td border=\"0\" style=\"text-align: center; padding: 25px 0 80px 0; color: #333333; background-color: #fff; font-family: 'Roboto', sans-serif; font-size: 12px; line-height: 1.25;\">
+                            *Trata-se de uma peça única (apenas BASE DE ADF PARA RICOH SP 3510SF M1191651), sendo<br>
+                            vendido de forma unitária. Não conta com o restante do ADF assim como conjunto de<br>
+                            engrenagens, plásticos que os envolve e demais itens da impressora.
+                        </td>
+                    </tr>
+
+                    <tr style=\"background-color: #333333;\" width:\"680\" cellspacing=\"0\" valign=\"top\" border=\"0\">
+                        <td border=\"0\">
+                            <table style=\"padding: 40px 0; margin: 0 auto; color: #f1f1f1; font-family: 'Roboto', sans-serif; font-size: 12px; font-weight: 400; line-height: 1.25;\">
+                                    <tr>
+                                        <td style=\"padding: 10px 50px 0 0;\">
+                                            <span style=\"color: #b3b2b2;\">A sua escolha em</span><br>
+                                            suprimentos para impressão<br><br>
+                                            
+                                            <img src=\"https://raw.githubusercontent.com/dan1el074/dan1el074/main/.github/workflow/xpert-logo-laranja.png\" style=\"padding-top: 5px;\" width=\"85\">
+                                        </td>
+                                        
+                                        <td style=\"padding-right: 50px; vertical-align: top;\">
+                                            <table cellpadding=\"0\" cellspacing=\"0\" width=\"100%\">
+                                                <tr>
+                                                    <td style=\"padding-bottom: 10px\">
+                                                        <span style=\"color: #fff\">Links úteis</span><br>
+                                                    </td>
+                                                </tr>
+
+                                                <tr>
+                                                    <td style=\"line-height: 1.7\">
+                                                        <img src=\"https://raw.githubusercontent.com/dan1el074/dan1el074/main/.github/workflow/arrow-orangered_1.png\" width=\"10\">
+                                                        <a href=\"https://xpertprint.com.br\" target=\"_blank\" style=\"text-decoration: none; color: #f1f1f1\">Site Institucional</a><br>
+                                                    
+                                                        <img src=\"https://raw.githubusercontent.com/dan1el074/dan1el074/main/.github/workflow/arrow-orangered_1.png\" width=\"10\">
+                                                        <a href=\"https://loja.xpertprint.com.br\" target=\"_blank\" style=\"text-decoration: none; color: #f1f1f1\">E-commerce</a><br>
+            
+                                                        <img src=\"https://raw.githubusercontent.com/dan1el074/dan1el074/main/.github/workflow/arrow-orangered_1.png\" width=\"10\">
+                                                        <a href=\"https://api.whatsapp.com/send?phone=5554992739630\" target=\"_blank\" style=\"text-decoration: none; color: #f1f1f1\">Whatsapp</a><br>
+                                                    </td>
+                                                </tr>
+                                            </table>
+                                        </td>
+                                        
+                                        <td style=\"vertical-align: top;\">
+                                            <table cellpadding=\"0\" cellspacing=\"0\" width=\"100%\">
+                                                <tr>
+                                                    <td style=\"padding-bottom: 10px\">
+                                                        <span style=\"color: #fff\">Contato</span><br>
+                                                    </td>
+                                                </tr>
+
+                                                <tr>
+                                                    <td style=\"line-height: 1.7\">
+                                                        <span color: #f1f1f1\">Telefone: (54) 99273-9630</span><br>
+                                                        E-mail: <a href=\"mailto:vendas@xpertprint.com.br\" style=\"text-decoration: none; color: #f1f1f1\">vendas@xpertprint.com.br</a><br>
+
+                                                        <a href=\"https://www.instagram.com/\" target=\"_blank\"><img src=\"https://raw.githubusercontent.com/dan1el074/dan1el074/main/.github/workflow/insta_1.png\" width=\"30\" style=\"margin-top: 10px\"></a>
+                                                        <a href=\"https://www.linkedin.com/\" target=\"_blank\"><img src=\"https://raw.githubusercontent.com/dan1el074/dan1el074/main/.github/workflow/lkdin.png\" width=\"30\" style=\"margin-top: 10px\"></a>
+                                                    </td>
+                                                </tr>
+                                            </table>
+                                        </td>
+                                    <tr>
+                            </table>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td style=\"background-color: #f1f1f1;\" width=\"100\">
+                            &nbsp;
+                        </td>
+                    </tr>
+                </table>
+            </body>
+        </html>
+    ";
+
+    $headers = array(
+        'From' => 'noreply@xpertprint.com.br',
+        'Reply-To' => 'no-reply@xpertprint.com.br',
+        'Content-type' => 'text/html',
+        'X-Mailer' => 'PHP/' . phpversion()
+    );
+
+    $sucesso = "";
+    $script = "";
+
+    if(isset($_POST['acao'])){
+        $email = nl2br($_POST['email']);
+        $to = str_replace('<br />', ',', $email);
+        $to = str_replace(';', ',', $to);
+
+        @mail($to, $subject,$messege, $headers) or die('Erro, e-mail não enviado!');
+
+        $mostraEmail = str_replace(';', '<br />', $email);
+        
+        $sucesso = "<strong>E-mail enviado para:</strong><br />$mostraEmail";
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>e-mail | Xpert</title>
+    </head> 
+
+    <body>
+        <style>
+            @import url('https://fonts.googleapis.com/css2?family=Roboto+Flex&display=swap');
+
+            .geral {  
+                display: flex;
+                flex-direction: row;
+                justify-content: center;
+                gap: 40px;
+                background-color: #f1f1f1;
+            }
+
+            .email {
+                margin-left: 40px;
+            }
+
+            .principal {
+                text-align: center;
+                font-family: 'Roboto Flex', sans-serif;
+            }
+
+            form {
+                display:flex;
+                flex-direction: column;
+                align-items: center;
+                gap: 18px;
+                margin: 40px 40px 0 0;
+            }
+
+            form div {
+                text-align: initial;
+            }
+
+            textarea {
+                padding: 15px;
+                margin-top: 10px;
+                width: 375px;
+            }
+
+            button {
+                background-color: #e56520;
+                color: #fff;
+                padding: 10px 16px;
+                border: 2px solid #e56520;
+                border-radius: 10px;
+                cursor: pointer;
+            }
+
+            button:hover {
+                background-color: #fff;
+                color: #e56520;
+            }
+
+            .sucesso {
+                max-width: 418px;
+                margin: 30px 0;
+            }
+
+            .img {
+                display: none;
+            }
+
+            .img img {
+                width: 100%;
+            }
+
+            @media (max-width: 1184px) {
+                .geral {  
+                    flex-direction: column;
+                    gap: 0;
+                    width: 100%;
+                }
+
+                .email {
+                    display: none;
+                }
+
+                .img {
+                    display: inline;
+                    margin: 0 auto;
+                    padding: 40px;
+                }
+
+                form {
+                    margin: 30px auto 0;
+                }
+
+                .sucesso {
+                    max-width: 100%;
+                }
+            }
+
+            @media (max-width: 693px) {
+                .principal {
+                    width: 100%;
+                }
+            }
+
+            @media (max-width: 490px) {
+                textarea {
+                    width: 300px;
+                }
+            }
+
+            @media (max-width: 400px) {
+                textarea {
+                    width: 250px;
+                }
+            }
+
+            @media (max-width: 330px) {
+                textarea {
+                    width: 200px;
+                }
+            }
+
+        </style>
+
+        <div class="geral">
+            <div class="email">
+                <?php echo $messege; ?>
+            </div>
+
+            <div class="img">
+                <img src="ft-tampa.png" alt="">
+            </div>
+            
+            <div class="principal">
+                <form method="POST">
+                    <div>
+                        <label>Endereços de e-mail: </label><br>
+                        <textarea placeholder="exemplo1@gmail.com, exemplo2@gmail.com, ..." name="email" rows="15" wrap="hard"></textarea>
+                    </div>
+                    <button name="acao">Enviar</button>
+                </form>
+
+                <div class="sucesso">
+                    <?php 
+                        echo $sucesso;
+                        echo $script;
+                    ?>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
