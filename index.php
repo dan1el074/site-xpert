8b355d86ff0333a3c7be072f959a875e14f2c094
Daniel Vargas <daniel.rvargas10@gmail.com>
2023-06-05 09:25:42 -0300

    mudança do nome do css

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="css/base.css">
-    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/main27.css">
 
     <link rel="apple-touch-icon" sizes="180x180" href="images/icons/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="images/icons/favicon-32x32.png">
@@ -20,10 +20,6 @@
         </div>
 
         <header class="s-header">
-            <a href="https://api.whatsapp.com/send?phone=5554992739630" class="btn-whatsapp-pulse btn-whatsapp-pulse-border" target="_blank">
-                <i class="fab fa-whatsapp"></i>
-            </a>
-
             <div class="header-logo">
                 <a class="site-logo" href="">
                     <img src="images/logo.svg" alt="Homepage">
@@ -34,8 +30,8 @@
                 <ul class="header-main-nav">
                     <li class="current"><a class="smoothscroll" href="#home" title="Home">Home</a></li>
                     <li><a class="smoothscroll" href="#produtos" title="Produtos">Produtos</a></li>
-                    <li><a class="smoothscroll" href="#sobre" title="Sobre">Sobre</a></li>
                     <li><a class="smoothscroll" href="#contato" title="Contato">Contato</a></li>	
+                    <li><a href="https://loja.xpertprint.com.br/" target="_blank" title="Loja" style="color: #e7651e;">Loja</a></li>
                 </ul>
             </nav>
 
@@ -62,6 +58,10 @@
                 <div class="intro-grid"></div>
                 <div class="intro-pic"></div>
             </div>
+
+            <a href="https://api.whatsapp.com/send?phone=5554992739630" class="btn-whatsapp-pulse btn-whatsapp-pulse-border" target="_blank">
+                <i class="fab fa-whatsapp"></i>
+            </a>
         </section>
 
         <section id="produtos" class="s-works target-section">
@@ -74,7 +74,7 @@
 
                     <div class="img-post">
                         <img src="images/base-adf-m1191651-1.png">
-                        <a href="https://api.whatsapp.com/send?phone=5554992739630">FAÇA SEU PEDIDO</a>
+                        <a href="https://loja.xpertprint.com.br/base-de-adf-p-ricoh-sp-3510sf-m1191651" target="_blank">FAÇA SEU PEDIDO</a>
                     </div>
                 </div>
             </div>
@@ -84,38 +84,24 @@
             <div class="about-me">
                 <div class="row heading-block" data-aos="fade-up">
                     <div class="column large-full">
-                        <h2 class="section-heading">Informações</h2>
+                        <h2 class="section-heading section-heading--centerbottom">Informações</h2>
                     </div>
                 </div>
 
                 <div class="row about-me__content" data-aos="fade-up">
-                    <div class="column large-full about-me__text">
+                    <div class="column large-full">
                         <p class="lead">
-                        Nosso primeiro produto, BASE DE ADF PARA RICOH SP 3510SF M1191651, é feito 
-                        em outro país e depois exportado para o Brasil. Sendo um produto paralelo, 
-                        garante qualidade assim como o original e em seus moldes para servir em 
-                        qualquer impressora do modelo RICOH 3510SF.
+                            Nosso primeiro produto, BASE DE ADF PARA RICOH SP 3510SF M1191651, é feito 
+                            em outro país e depois exportado para o Brasil. Sendo um produto paralelo, 
+                            garante qualidade assim como o original e em seus moldes para servir em 
+                            qualquer impressora do modelo RICOH 3510SF.
                         </p>
 
                         <p>
-                        *Vale lembrar que se trata de uma peça única (apenas BASE DE ADF PARA RICOH 
-                        SP 3510SF M1191651), sendo vendido em uma caixa com 10 unidades e não de 
-                        forma unitária. Não conta com o restante do ADF assim como conjunto de 
-                        engrenagens, plásticos que os envolve e demais itens da impressora.
-                        </p>
-
-                        <p>
-                        A Xpert - Comércio de Equipamentos de Impressão, é uma empresa localizada na 
-                        região norte do Rio Grande de Sul e tem como principal objetivo oferecer 
-                        produtos relacionados a impressoras que, por muitas vezes, não se encontra 
-                        disponível em nosso mercado nacional.
-                        </p>
-
-                        <p>
-                        Contando com anos de experiência na área, sabemos exatamente a sua necessidade 
-                        e trabalhamos com seriedade, oferecendo garantia, nota fiscal de todos os 
-                        produtos que temos disponível em nosso estoque e atendemos demandas para todo o 
-                        Brasil.
+                            *Vale lembrar que se trata de uma peça única (apenas BASE DE ADF PARA RICOH 
+                            SP 3510SF M1191651), sendo vendido em uma caixa com 10 unidades e não de 
+                            forma unitária. Não conta com o restante do ADF assim como conjunto de 
+                            engrenagens, plásticos que os envolve e demais itens da impressora.
                         </p>
                     </div>
                 </div>
@@ -132,8 +118,9 @@
             <div class="row testimonials" data-aos="fade-up">
                 <div class="column large-full testimonials__slider">
                     <div class="testimonials__slide">
-                        <p>“Atuando diariamente no setor de suporte à impressoras, por diversas vezes nos deparamos sem peças para reposição em equipamentos
-                            danificados, neste contexto encontramos a Xpert, com peças de alta qualidade e durabilidade.”</p>
+                        <p>“Atuando diariamente no setor de suporte à impressoras, por diversas vezes nos 
+                            deparamos sem peças para reposição em equipamentos danificados, neste contexto 
+                            encontramos a Xpert, com peças de alta qualidade e durabilidade.”</p>
                         <div class="testimonials__info">
                             <img src="images/avatars/lucas.jpg" alt="Author image" class="testimonials__avatar">
                             <cite class="testimonials__cite">
@@ -146,38 +133,89 @@
             </div>
         </section>
 
-        <section id="contato" class="s-contact target-section">
-            <div class="row heading-block" data-aos="fade-up">
-                <div class="column large-full">
-                    <h2 class="section-heading">ENTRE EM CONTATO</h2>
-                </div>
-            </div>
+        <section id="produtos2" class="s-works target-section">            
+            <div class="row heading-block heading-block--center" data-aos="fade-up">
 
-            <div class="row contact-main" data-aos="fade-up">
                 <div class="column large-full">
-                    <p class="section-desc email">
-                        <a href="mailto:comercial@xpertprint.com.br">comercial@xpertprint.com.br</a>
-                    </p>
+                    <h2 class="section-heading section-heading--centerbottom">Mais Produtos</h2>
 
-                    <p class="section-desc">
-                        Entre agora em contato com nossa equipe comercial pelo <span class="f-email">e-mail</span><a href="mailto:comercial@xpertprint.com.br" class="f-email-mobile">e-mail</a>, 
-                        whatsapp ou pelas nossas redes sociais.
+                    <p class="center">
+                        Além das peças que produzimos, também atuamos como revenda de outros produtos como 
+                        toners para impressoras monocromáticas, refis de tinta para impressoras colorida, e muito mais.
                     </p>
                 </div>
             </div>
         </section>
 
-        <footer>
-            <div class="row">
+        <footer id="contato">
+            <section class="primeira">
                 <div class="column large-full ss-copyright">
-                    <span>© Copyright 2022 - Xpert Comercio de Equipamentos de Impressão LTDA </span> 
-                    <span>Design by <a>Equipe de Desenvolvimento</a></span>
-                </div>            
-            </div>
+                    <div>
+                        <div class="footer-atendimento">
+                            <p><strong>Atendimento</strong></p>
+                            <p><strong style="font-size: 20px;">(54) 99273-9630</strong></p>
+                            <p>Seg a Sex - das 8h às 18h</p>
+
+                            <img src="images/logo.svg" alt="Homepage">
+                        </div>
+
+                        <div class="footer-endereco">
+                            <p><strong>XPERT SUPRIMENTOS PARA IMPRESSAO LTDA</strong></p>
+                            <p>
+                                CNPJ: 49.497.127/0001-57<br>
+                                Rua Marcelino Ramos, 297 - Sala Comercial<br>
+                                CEP: 99700-074 - Centro<br>
+                                Erechim - RS
+                            </p>
+                        </div>
+                    </div>
+
+                    <div>
+                        <div class="footer-sobre">
+                            <p><strong>Sobre</strong></p>
+                            
+                            <p>
+                                A Xpert - Comércio de Equipamentos de Impressão, é uma empresa localizada na região norte 
+                                do Rio Grande de Sul e tem como principal objetivo oferecer produtos relacionados a impressoras 
+                                que, por muitas vezes, não se encontra disponível em nosso mercado nacional.
+                            </p>
+
+                            <p>
+                                Contando com anos de experiência na área, sabemos exatamente a sua necessidade e trabalhamos com 
+                                seriedade, oferecendo garantia, nota fiscal e atendemos demandas para todo o Brasil.
+                            </p>
+                        </div>
+
+                        <div class="footer-sites">
+                            <p><strong>Institucional</strong></p>
+
+                            <a href="https://loja.xpertprint.com.br/" target="_blank">Nosso e-commerce</a>
+                            <a href="#">Mais sobre nós</a>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <section class="segunda">
+                <div>
+                    <p><strong>Selos</strong></p>
+
+                    <div>
+                        <img src="images/selo-ssl.svg" alt="Certificado SSL" title="Certificado SSL">
+                        <img src="images/google-safe-selo.webp" alt="Google - Site Seguro" title="Google - Site Seguro">
+                    </div>
+                </div>
+            </section>
+
+            <section class="terceira">
+                <p>
+                    XPERT SUPRIMENTOS PARA IMPRESSAO LTDA | CNPJ: 49.497.127/0001-57
+                    Rua Marcelino Ramos, 297 - Sala Comercial - Centro, Erechim - RS, 99700-074
+                </p>
+            </section>
         </footer>
 
         <script src="js/modernizr.js"></script>
-        <script src="js/pace.min.js"></script>
         <script src="js/jquery-3.2.1.min.js"></script>
         <script src="js/plugins.js"></script>
         <script src="js/main.js"></script>
